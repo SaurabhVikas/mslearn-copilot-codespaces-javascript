f8772df49ddf9ea2591ab40822a6df9951a0ac2f
SaurabhVikas <99705771+SaurabhVikas@users.noreply.github.com>
2025-12-28 13:42:27 +0000

    Update siteProps with personal information and enhance social icon styles

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Saurabh Vikas",
+  title: "Trying hard!",
+  email: "saurabh.vikas@gmail.com",
+  gitHub: "saurabhvikas",
+  instagram: "IDontHaveOne",
+  linkedIn: "saurabhvikas",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "IAmNotOnTwitter",
+  youTube: "saurabhvikas",
 };
 
 const primaryColor = "#4E567E";
